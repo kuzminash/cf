3afca0e8a2db00608b69c8c94ffc0868b23cf5f5
alexandrakuzmina <kuzminash@list.ru>
2020-02-24 17:06:55 +0300

    base functions

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <math.h>
+#include <bitset>
+#include <map>
+#include <deque>
+#include <queue>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pp;
+const int inf = (int)1e9;
+
+
+#define umap unordered_map
+#define Power ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+    Power;
+    
+    return 0;
+}
